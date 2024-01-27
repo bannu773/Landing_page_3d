86bd1ae1402596e14b25207614ffcc60577c007c
bannu773 <bandibannu773@gmail.com>
2024-01-27 18:42:44 +0530

    second

--- src/comp/land_3d/Land_3d.jsx
+++ src/comp/land_3d/Land_3d.jsx
@@ -3,7 +3,7 @@ import Spline from '@splinetool/react-spline';
 export default function Land_3d() {
   return (
     <div className='render_3d'>
-      <Spline scene="https://prod.spline.design/zyD1Dx-cIrlwU2TR/scene.splinecode" />
+      <Spline scene="https://prod.spline.design/sqefH0tO5t7porj6/scene.splinecode" />
 
     </div>
   );
--- src/comp/landing_page/Landing_page.jsx
+++ src/comp/landing_page/Landing_page.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Flex} from 'antd';
+import { Flex } from 'antd';
 import './landing_page.css'
 import Spline from '@splinetool/react-spline';
 import Land_3d from '../land_3d/Land_3d';
@@ -8,11 +8,11 @@ import Resume_review from '../Resume_review/Resume_review';
 
 const Landing_page = () => {
   return (
-    <div className='main_container '> 
-        <Land_3d />
-        <Resume_review />
-        <TimeLine />
-        
+    <div className='main_container '>
+      <Land_3d />
+      <Resume_review />
+      {/* <TimeLine /> */}
+
     </div>
   )
 }
